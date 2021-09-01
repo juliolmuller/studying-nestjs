24d57ad3d98f3601d8b871f2913eb2d890b8f1a7
Julio Muller <juliodolago@gmail.com>
2021-09-01 00:52:53 -0300

    Add mock data

---
+++ src/db.ts
@@ -0,0 +1,38 @@
+export const students = [
+  {
+    id: '8Mdt-x1lTN3GnpV8jE5p0',
+    name: 'Anthony Cole',
+    teacher: '0t_G2hRjQwMyCaEZdsIuC',
+  },
+  {
+    id: 'xqE-evqvZVa-mwPm8HbvC',
+    name: 'Micheal Bryant',
+    teacher: '0t_G2hRjQwMyCaEZdsIuC',
+  },
+  {
+    id: 'YhFBrmoBbdLCi-6GWD7yK',
+    name: 'Kobe Jordan',
+    teacher: '0t_G2hRjQwMyCaEZdsIuC',
+  },
+  {
+    id: 'SGi26_JU9AfpOt2Jw6Rzj',
+    name: 'Laith Harb',
+    teacher: 'QBw9c0EV-eeQlMK2J4L3O',
+  },
+  {
+    id: 'wL1iH8M-TWCnnz_51ThRA',
+    name: 'Kendrick Jenner',
+    teacher: 'QBw9c0EV-eeQlMK2J4L3O',
+  },
+];
+
+export const teachers = [
+  {
+    id: '0t_G2hRjQwMyCaEZdsIuC',
+    name: 'Ms Jackson',
+  },
+  {
+    id: 'QBw9c0EV-eeQlMK2J4L3O',
+    name: 'Mr Wade',
+  },
+];
